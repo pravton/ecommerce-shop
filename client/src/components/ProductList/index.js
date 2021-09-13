6c085bde245a4e87f346afacd2541ac2a26351d1
Clinton <80900245+pravton@users.noreply.github.com>
2021-09-13 17:32:09 -0400

    added indexedDB and offline functionalities

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 
+import { idbPromise } from '../../utils/helpers';
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
@@ -8,20 +9,6 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 
 function ProductList() {
-  // const { loading, data } = useQuery(QUERY_PRODUCTS);
-
-  // const products = data?.products || [];
-
-  // function filterProducts() {
-  //   if (!currentCategory) {
-  //     return products;
-  //   }
-
-  //   return products.filter(
-  //     (product) => product.category._id === currentCategory
-  //   );
-  // }
-
   const [state, dispatch] = useStoreContext();
 
   const { currentCategory } = state;
@@ -34,8 +21,22 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      // save it i the indexedDB using helper funtion
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) {
+      // since we're offline, get all of the data from the 'products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrived data set to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      })
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if(!currentCategory) {
